de01f79b3173d55d4cb320b90b9f22d5b294ea3b
Lseungjin <promotion0602@naver.com>
2023-11-28 21:09:37 +0900

    버튼수정

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,46 +9,62 @@ class Main extends Component{
                 <img src={mvp} alt="Logo" className="logo" />
                 <table className = 'table text-muted text-center'>
                     <thead>
-
                         <tr style={{color:'black'}}>
-                        
                             <th scope='col'>사용자 총 투표 횟수</th>
                             <th scope='col'>투표 종료 횟수 </th>
-
                         </tr>
-
                     </thead>
                     <tbody>
                         <tr style={{color:'black'}}>
                             <td>{window.web3.utils.fromWei(this.props.stakingBalance, 'Ether')} 회</td>
                             {/* replace count function ... 수정 필요 */}
                             <td>{this.props.unstakeCount} 회</td>
-                            {/*<td>{window.web3.utils.fromWei(this.props.rwdBalance, 'Ether')} RWD</td>*/}
                         </tr>
                     </tbody>
                 </table>
-                    <span className='balance-text'>
-                        투표 종료까지 남은 투표수: {window.web3.utils.fromWei(this.props.mvpBalance, 'Ether')}
-                    </span>
+                <span className='balance-text'>
+                    투표 종료까지 남은 투표수: {window.web3.utils.fromWei(this.props.mvpBalance, 'Ether')}
+                </span>
                 <div>
-                    <div className='button-container'>
-                        <button 
-                            onClick={(event) =>{
-                                event.preventDefault()
-                                let amount
-                                amount = window.web3.utils.toWei('1', 'Ether')
-                                this.props.stakeTokens(amount)
-                            }}
-                            className='btn btn-send action-btn'>사용자 총 투표 횟수 증가시키기</button>
-                        <button 
-                            onClick={(event) =>{
-                                event.preventDefault(
-                                    this.props.unstakeTokens()
-                                    //this.props.issueTokens()
-                                )
-                            }}
-                            className='btn btn-take action-btn'>투표 종료 횟수 증가시키기</button>
+                <div className='card mb-2' style={{opacity:'.9'}}>
+    <form 
+        onSubmit={(event) =>{
+            event.preventDefault()
+            let amount
+            amount = this.input.value.toString()
+            amount = window.web3.utils.toWei(amount, 'Ether')
+            this.props.stakeTokens(amount)
+        }}
+        className='mb-3'>
+        <div style={{borderSpacing:'0 1em'}}>
+            <label className='float-left' style={{marginLeft:'15px'}}><b>증가시킬 투표 횟수</b></label>
+            <span className='float-right' style={{marginRight:'8px'}}>
+            남은 MVP토큰: {window.web3.utils.fromWei(this.props.mvpBalance, 'Ether')} 개
+            </span>
+            <div className='input-group mb-4'>
+                <input
+                ref={(input) => {this.input = input} }
+                type='text'
+                placeholder='0'
+                required/>
+                <div className='input-group-append'>
+                    <div className='input-group-text'>
+                        &nbsp;&nbsp;&nbsp;회
                     </div>
+                </div>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <button type='submit' className='btn btn-send action-btn'>사용자 총 투표 횟수 증가시키기</button>
+                <button 
+                    onClick={(event) =>{
+                        event.preventDefault()
+                        this.props.unstakeTokens()
+                    }}
+                    className='btn btn-take action-btn'>투표 종료 횟수 증가시키기</button>
+            </div>
+        </div>
+    </form>
+</div>
                     <div className='button-container' style={{ justifyContent: 'center' }}>
                         <button
                             onClick={() => window.location.href='https://sony-babba.vercel.app/admin'}
@@ -59,6 +75,5 @@ class Main extends Component{
         )
     }
 }
-
 
 export default Main;
